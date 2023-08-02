7426d2c93868cab2c16682c52b171a0f0d5d3130
Keith koki <kokikeith@gmail.com>
2023-08-01 23:57:04 -0700

    file fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,7 +32,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (FALSE);
+		return (0);
 
 	if (binary_tree_size(tree) > 0)
 		return (1);
