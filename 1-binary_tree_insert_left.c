5da8d189495078c55224fcd78aed86937809783d
Keith koki <kokikeith@gmail.com>
2023-08-02 00:15:00 -0700

    file fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,19 +21,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (parent->left != NULL)
-	{
-		parent->left->parent = new_node;
-		new_node->left = parent->left;
-	}
-	else
-	{
-		new_node->left = NULL;
-	}
-
-	parent->left = new_node;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	new_node->parent = parent;
 	new_node->n = value;
+
+	if (parent->left == NULL)
+		parent->left = new_node;
+
+	else
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+		parent->left = new_node;
+	}
 
 	return (new_node);
 }
